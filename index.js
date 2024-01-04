41a3f5d6765062d5fa2739366ed6e1e804262a49
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-01-04 16:37:00 +0300

    Completed Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const names = ["Spencer", "Curtis", "Becky"];
+const eventName = "anniversary";
+function writeCards(names, eventName) {
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+
+function countDown(numStart) {
+    for (let i = numStart; i >= 0; i--) {
+        console.log(i);
+    }
+}
+countDown(15);
+
+function countDown1(numStart1) {
+    while (numStart1 >= 0) {
+        console.log(numStart1);
+        numStart1--;
+    }
+}
+countDown1(10);
